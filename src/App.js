2966e21da426d780e6937c4fae3baa7886e7c983
vorujack <vorujack@gmail.com>
2021-11-14 18:01:44 +0330

    version 10

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import Routes from './Routes';
-import Layout from './Layout';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './App.css';
-import { memo } from 'react';
+import Routes from "./Routes";
+import Layout from "./Layout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import "./assets/style/style.css";
+import { memo } from "react";
 
 const App = memo(() => {
-  return (<>
-    <Layout>
-      <div id="main-content">
-        <Routes />
-      </div>
-    </Layout>
-  </>);
+  return (
+    <>
+      <Layout>
+        <div id="main-content">
+          <Routes />
+        </div>
+      </Layout>
+    </>
+  );
 });
 
 export default App;
